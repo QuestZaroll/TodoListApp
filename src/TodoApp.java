254b160cff43966b33830503dce4313f67658c7f
QuestGiver <armyof.quest@gmail.com>
2023-02-08 21:43:40 -0500

    change test data init and add focus listener for items

--- src/TodoApp.java
+++ src/TodoApp.java
@@ -1,10 +1,7 @@
 import javax.swing.*;
 import java.awt.*;
 import java.util.ArrayList;
-
-//implement GUI and App logic here
 /*
-
 --parameters for completion
 app should be able to save, load, and create new to-do lists.
 new items should be able to be added either through a menu option or right-click.
@@ -40,10 +37,23 @@ public class TodoApp {
 
     public TodoApp(){
 
-        for (int i = 0; i < 25; i++) {
+        //I know this is a terrible implementation of fizzbuzz, just making some test data for task nesting
+        for (int i = 0; i < 30; i++) {
             TodoItem item = new TodoItem("test item number: " + (i + 1), "test description: " + (i + 1));
+            if((i + 1) % 3 == 0){
+                TodoItem childFizz = new TodoItem("Child fizz for item: " + (i + 1), "Fizz!");
+                item.addChild(childFizz);
+                if((i + 1) % 5 == 0){
+                    TodoItem childFizzBuzz = new TodoItem("child fizzbuzz for item: " + (i + 1), "FizzBuzz!");
+                    childFizz.addChild(childFizzBuzz);
+                }
+            }else if ((i + 1) % 5 == 0){
+                TodoItem childBuzz = new TodoItem("child buzz for item: " + (i + 1), "Buzz!");
+                item.addChild(childBuzz);
+            }
             items.add(item);
         }
+
         for (int i = 0; i < items.toArray().length; i++) {
             itemsModel.addElement(items.get(i));
         }
@@ -52,22 +62,26 @@ public class TodoApp {
         frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         frame.setSize(420, 650);
         frame.setResizable(true);
-
-
-
-        itemsPanel.setLayout(new BoxLayout(itemsPanel, BoxLayout.Y_AXIS));
-        itemsPanel.setBackground(Color.BLUE);
+        frame.setLocationRelativeTo(null);
 
         JScrollPane scrollPane = new JScrollPane(itemsList);
         scrollPane.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_AS_NEEDED);
         scrollPane.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_AS_NEEDED);
+
+        itemsPanel.setLayout(new BoxLayout(itemsPanel, BoxLayout.Y_AXIS));
         itemsPanel.add(scrollPane);
+
+        itemsList.setBackground(Color.GRAY);
 
         descriptText.setEditable(false);
         descriptText.setLineWrap(true);
         descriptText.setWrapStyleWord(true);
+        descriptText.setBackground(Color.DARK_GRAY);
+        descriptText.setForeground(Color.BLACK);
+
+        descriptText.setFont(new Font("Consolas", Font.PLAIN, 40));
+
         descriptPanel.setLayout(new BoxLayout(descriptPanel, BoxLayout.Y_AXIS));
-        descriptPanel.setBackground(Color.RED);
         descriptPanel.add(descriptText);
 
         JSplitPane splitPane = new JSplitPane(
@@ -76,9 +90,28 @@ public class TodoApp {
                 descriptPanel);
 
         splitPane.setResizeWeight(0.5);
+        splitPane.setDividerSize(2);
 
         frame.add(splitPane);
 
         frame.setVisible(true);
+
+        itemsList.addListSelectionListener(e -> {
+            if (!e.getValueIsAdjusting()) {
+                TodoItem selectedItem = itemsList.getSelectedValue();
+                descriptText.setText(selectedItem.getDescription());
+
+                if (!selectedItem.getChildren().isEmpty()){
+                    ArrayList<TodoItem> testList = selectedItem.getFamily();
+                    for (TodoItem t : testList) {
+                        System.out.println(t.getTask() + ";\n Description: " + t.getDescription());
+                    }
+                }
+            }
+        });
+    }
+
+    public void displayChildren(){
+
     }
 }
